f5e63dcc229aea0762adeb1178a8c607b18e56aa
Angel Ng <ang7@stuy.edu>
2017-04-21 10:35:58 -0400

    Ticket class

---
+++ Ticket.java
@@ -0,0 +1,57 @@
+public class Ticket extends Comparable{
+    private int id; 
+    private int VIPLevel; 
+    private String problem; 
+    private boolean b; 
+    private String name; 
+    private String soln; 
+
+    public Ticket(int num, int level, String prob, String client){
+	id = num; 
+	VIPLevel = level; 
+	problem = prob; 
+	b = false; 
+	name = client; 
+	soln = null; 
+    }
+
+    public int getID(){
+	return id; 
+    }
+
+    public int getVIP(){
+	return VIPLevel; 
+    }
+
+    public String getProblem(){
+	return problem; 
+    }
+    
+    public String getName(){
+	return name; 
+    }
+
+    public boolean isSolved(){
+	return b;
+    }
+    
+    public void done(){
+	b = true; 
+    }
+
+    public void setSoln(String solution){
+	soln = solution; 
+    }
+
+    public int compareTo(Ticket other){
+	if ( this.getVIP() < other.getVIP() ){
+	    return -1; 
+	}
+	else if ( this.getVIP() > other.getVIP() ){
+	    return 1; 
+	}
+	else{
+	    return 0; 
+	}
+    }
+}
